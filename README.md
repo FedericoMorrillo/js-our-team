39dbe435c2faa0bc9d2572641407fdef87c92034
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-20 21:19:23 +0100

    feat: stampa degli oggetti in console

---
+++ README.md
@@ -0,0 +1,7 @@
+# Our Team
+
+1) dichiariamo un array dove all' interno contiene oggetti che rappresenteranno i membri del team;
+3) stampiamo su console il risultato grazie ad un ciclo con all' interno un ciclo per le key;
+4) stampiamo all' interno del DOM il risultato;
+5) convertiamo la stringa di immagini in vere immagini;
+6) creiamo delle card dove all' interno inseriamo i dati di ogni utente;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-'use strict'
+"use strict";
+
+//dichiarazione oggetti in un array
+const team = [
+  {
+    "nome-cognome": "Wayne Barnett",
+    professione: "Founder & CEO",
+    fotoprofilo: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    "nome-cognome": "Angela Caroll",
+    professione: "Chief Editor",
+    fotoprofilo: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    "nome-cognome": "Walter Gordon",
+    professione: "Office Manager",
+    fotoprofilo: "walter-gordon-office-manager.jpg",
+  },
+  {
+    "nome-cognome": "Angela Lopez",
+    professione: "Social Media Manager",
+    fotoprofilo: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    "nome-cognome": "Scott Estrada",
+    professione: "Developer",
+    fotoprofilo: "scott-estrada-developer.jpg",
+  },
+  {
+    "nome-cognome": "Barbara Ramos",
+    professione: "Graphic Designer",
+    fotoprofilo: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+
+for(let i=0; i<team.length; i++){
+    const utente = team[i];
+    //console.log(team[i]);
+    for(let key in utente){
+        console.log(`${key}: ${utente[key]}`);
+    }
+     console.log("**********************************")
+}
